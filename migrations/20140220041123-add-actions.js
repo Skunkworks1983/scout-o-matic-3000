6309a7a6f30f8441a59c3e4c39581af473f07103
AWOL <skunk.awol@gmail.com>
2014-02-22 11:27:29 -0800

    pg migrations

---
+++ migrations/20140220041123-add-actions.js
@@ -0,0 +1,22 @@
+var dbm = require("db-migrate");
+var type = dbm.dataType;
+
+exports.up = function(db, next) {
+	db.createTable("actions", {
+		"id": { "type": "int", "primaryKey": true },
+		"action": "string",
+		"value": "string",
+		"x": "int",
+		"y": "int",
+		"time": "int",
+		"event_id": "string",
+		"team_number": "int",
+		"match_number": "int",
+		"scout_number": "int",
+		"scout_name": "string"
+	}, next);
+};
+
+exports.down = function(db, next) {
+	db.dropTable("actions", next);
+};
