2841ff60054c3cdb5f3601831280b6f38d30b47d
Skunk AWOL <skunk.awol@gmail.com>
2014-04-24 04:41:38 -0700

    Newton

--- scout.js
+++ scout.js
@@ -2,7 +2,7 @@ var express = require("express");
 var pg = require("pg").native;
 var async = require("async");
 
-var whatEventIsHappeningRightNow = "2014pncmp";
+var whatEventIsHappeningRightNow = "2014new";
 var whatEventIsHappeningRightNowName = "";
 
 var cache = require("./tba.js");
